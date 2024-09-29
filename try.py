92a1c8b377e49b81e6266dbce1f681b881052e91
Christopher Argenis Preciado Silva <138161574+CAPS2125@users.noreply.github.com>
2024-09-28 20:22:05 -0700

    Import and try with html document
    Importación de la clase render_template e intento de desplegarlo. Fallido.

--- try.py
+++ try.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World</h1>'
+    return render_template("hello_html.html")
 
 if __name__ == '__main__':
     app.run()
